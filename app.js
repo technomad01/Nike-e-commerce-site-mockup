4fa32dc94f8f9c9c7f3461d4451c085fc95b6cc1
Claudia <153988396+technomad01@users.noreply.github.com>
2024-04-12 13:53:22 +0300

    Update app.js
    combined several functions for optimizations

--- app.js
+++ app.js
@@ -1,137 +1,110 @@
+// Function to set up the slider functionality
+function setupSlider(wrapper, menuItems, products) {
+  menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      //change the current slide
+      wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+      //change the choosen product
+      choosenProduct = products[index];
+
+      // Update product details
+      displayProductDetails(choosenProduct);
+    });
+  });
+}
+
+// Function to display product details
+function displayProductDetails(product) {
+  currentProductTitle.textContent = product.title;
+  currentProductPrice.textContent = "$" + product.price;
+  currentProductImg.src = product.colors[0].img;
+
+  // Assign new colors
+  currentProductColors.forEach((color, index) => {
+    color.style.backgroundColor = product.colors[index].code;
+  });
+}
+
+// Function to set up event listeners
+function setupEventListeners() {
+  // Event listener for color selection
+  currentProductColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+      currentProductImg.src = choosenProduct.colors[index].img;
+    });
+  });
+
+  // Event listener for size selection
+  currentProductSizes.forEach((size, index) => {
+    size.addEventListener("click", () => {
+      currentProductSizes.forEach((size) => {
+        size.style.backgroundColor = "white";
+        size.style.color = "black";
+      });
+      size.style.backgroundColor = "black";
+      size.style.color = "white";
+    });
+  });
+
+  // Event listener for product button
+  productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+  });
+
+  // Event listener for close button
+  close.addEventListener("click", () => {
+    payment.style.display = "none";
+  });
+}
+
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
-
-const products = [
-    {
-        id: 1,
-        title: "Air Force",
-        price: 119,
-        colors: [
-            {
-                code: "black",
-                img: "./img/air.png"
-            },
-            {
-                code: "dark blue",
-                img: "./img/air2.png"
-            },
-        ],      
-    },
-    {
-        id: 2,
-        title: "Jordan",
-        price: 149,
-        colors: [
-            {
-                code: "grey",
-                img: "./img/jordan.png"
-            },
-            {
-                code: "green",
-                img: "./img/jordan2.png"
-            },
-        ],      
-    },
-    {
-        id: 3,
-        title: "Blazer",
-        price: 109,
-        colors: [
-            {
-                code: "white",
-                img: "./img/blazer.png"
-            },
-            {
-                code: "green",
-                img: "./img/blazer2.png"
-            },
-        ],      
-    },
-    {
-        id: 4,
-        title: "Crater",
-        price: 129,
-        colors: [
-            {
-                code: "black",
-                img: "./img/crater.png"
-            },
-            {
-                code: "white",
-                img: "./img/crater2.png"
-            },
-        ],      
-    },
-    {
-        id: 5,
-        title: "Hippie",
-        price: 99,
-        colors: [
-            {
-                code: "grey",
-                img: "./img/hippie.png"
-            },
-            {
-                code: "black",
-                img: "./img/hippie2.png"
-            },
-        ],      
-    },
-]
-
-let chosenProduct = products[0]
-
 const currentProductImg = document.querySelector(".productImg");
 const currentProductTitle = document.querySelector(".productTitle");
 const currentProductPrice = document.querySelector(".productPrice");
 const currentProductColors = document.querySelectorAll(".color");
-const currentProductSize = document.querySelectorAll(".size")
-
-menuItems.forEach((item, index)=> {
-    item.addEventListener('click' , ()=> {
-        //change the current slide 
-        wrapper.style.transform = `translateX(${-100 * index}vw)`;
-
-        // change the chosen product
-        chosenProduct = products[index]
-
-        // change text of currentProduct
-        currentProductTitle.textContent = chosenProduct.title;
-        currentProductPrice.textContent = "$" + chosenProduct.price;
-        currentProductImg.src = chosenProduct.colors[0].img;
-
-        //assign new colors
-        currentProductColors.forEach((color, index) => {
-            color.style.backgroundColor = chosenProduct.colors[index].code;
-        });
-    });
-});
-
-currentProductColors.forEach((color, index) => {
-    color.addEventListener('click', () => {
-        currentProductImg.src = chosenProduct.colors[index].img;
-    })
-})
-
-currentProductSize.forEach((size, index) => {
-    size.addEventListener('click', ()=> {
-        currentProductSize.forEach((size) => {
-            size.style.backgroundColor =' white'
-            size.style.color = 'black'
-        })
-        size.style.backgroundColor =' black'
-        size.style.color = 'white'
-    })
-})
-
+const currentProductSizes = document.querySelectorAll(".size");
 const productButton = document.querySelector(".productButton");
 const payment = document.querySelector(".payment");
 const close = document.querySelector(".close");
 
-productButton.addEventListener("click", () => {
-    payment.style.display = "flex"
-})
+//Product data
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+ 
+];
 
-close.addEventListener("click", () => {
-    payment.style.display = "none"
-})
+document.addEventListener("DOMContentLoaded", function () {
+  setupSlider(wrapper, menuItems, products);
+  setupEventListeners();
+});
